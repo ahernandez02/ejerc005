9abe6731abfa9c47ca4edd9ce9b0f81e9257969d
Name <you@example.com>
2023-07-26 02:25:17 +0200

    Test integracion

--- src/test/java/es/cic/LienzoIntegrationTest.java
+++ src/test/java/es/cic/LienzoIntegrationTest.java
@@ -1,60 +1,59 @@
 package es.cic;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.awt.Color;
-import java.io.IOException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
+import java.awt.Color;
+import java.io.IOException;
 
 import es.cic.Figuras.Circulo;
 import es.cic.Figuras.Cuadrado;
 import es.cic.Figuras.Linea;
 import es.cic.Figuras.Punto;
 
+
+
 class LienzoIntegrationTest {
 
-	private Lienzo lienzo;
+	private Lienzo cut;
 	
 	@BeforeEach
 	void setUp() throws Exception 
 	{
-		this.lienzo = new Lienzo();
+		this.cut = new Lienzo();
 	}
 	
-	/*
-	@Test
-	void testDibujarPunto() {
-		fail("Not yet implemented");
+	@Test 
+	void dibujarPunto()
+	{
+		Punto pt = new Punto(500, 850, new Color(255, 0, 0));
+		this.cut.dibujar(pt);	
+		
+		//Comprueba lista figuras
+		assertThat(this.cut.getFiguras().contains(pt), is(true));
+		
+		//Comprueba lienzo
+		assertThat(this.cut.getLienzo()[pt.x][pt.y], not(0));
 	}
-	@Test
-	void testDibujarLinea() {
-		fail("Not yet implemented");
-	}
-	
-	void testDibujarCuadrado() {
-		fail("Not yet implemented");
-	}
-	
-	void testDibujarCirculo() {
-		fail("Not yet implemented");
-	}
-	*/
 	
 	@Test
 	void testMostrarLienzo() throws IOException 
 	{
 		Punto pt = new Punto(500, 850, new Color(255, 0, 0));
-		this.lienzo.dibujar(pt);	
+		this.cut.dibujar(pt);	
 		Linea l = new Linea(500,800, new Color(0,255,0), 500);
-		this.lienzo.dibujar(l);
+		this.cut.dibujar(l);
 		Cuadrado c = new Cuadrado(500, 200, new Color(0,0,255), 10_000);
-		this.lienzo.dibujar(c);
+		this.cut.dibujar(c);
 		Circulo circ = new Circulo(500,500, new Color(255,0,0), 100);
-		this.lienzo.dibujar(circ);
+		this.cut.dibujar(circ);
 		
-		this.lienzo.mostrar();
+		this.cut.mostrar();
 	}
 	
 	/*
